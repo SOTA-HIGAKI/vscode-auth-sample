82c531e9daef2ffa89f94f8670ab9bee2629f89b
sota <souta0829win7@yahoo.co.jp>
2022-11-09 13:03:51 +0900

    add comment and set placeholder to InputBox

--- src/extension.ts
+++ src/extension.ts
@@ -22,6 +22,196 @@ import { PromiseAdapter, promiseFromEvent } from './util';
 
 const SESSIONS_SECRET_KEY = "test_key"
 
+// const AUTH_TYPE = `BeEn`;
+// const AUTH_NAME = `beEn`;
+
+// export class BeEnAuthenticationProvider implements AuthenticationProvider, Disposable {
+// 	private _sessionChangeEmitter = new EventEmitter<AuthenticationProviderAuthenticationSessionsChangeEvent>();
+// 	private _disposable: Disposable;
+// 	private _pendingStates: string[] = [];
+// 	private _codeExchangePromises = new Map<string, { promise: Promise<string>; cancel: EventEmitter<void>}>();
+// 	private _uriHandler = new UriEventHandler();
+
+// 	constructor(private readonly context: ExtensionContext) {
+// 		this._disposable = Disposable.from(
+// 			authentication.registerAuthenticationProvider(
+// 				'BeEn',
+// 				'BeEn',
+// 				this,
+// 				{ supportsMultipleAccounts: false }
+// 			),
+// 			window.registerUriHandler(this._uriHandler)
+// 		)
+// 	}
+
+// 	get onDidChangeSessions() {
+// 		return this._sessionChangeEmitter.event;
+// 	}
+
+// 	get redirectUri() {
+// 		const publisher = this.context.extension.packageJSON.publisher;
+// 		const name = this.context.extension.packageJSON.name;
+// 		return `${env.uriScheme}://${publisher}.${name}`;
+// 	}
+
+// 	/**
+// 	 * @param scopes
+// 	 * @returns
+// 	 */
+// 	public async getSessions(scopes?: string[]): Promise<readonly AuthenticationSession[]> {
+// 		const allSessions = await this.context.secrets.get(SESSIONS_SECRET_KEY);
+
+// 		if (allSessions) {
+// 			return JSON.parse(allSessions) as AuthenticationSession[];
+// 		}
+// 		return [];
+// 	}
+
+// 	/**
+// 	 *
+// 	 * @param scopes
+// 	 * @returns
+// 	 */
+// 	public async createSession(scopes: string[]): Promise<AuthenticationSession> {
+// 		try {
+// 			const token = await this.login(scopes);
+// 			if (!token) {
+// 				throw new Error('login failed');
+// 			}
+
+// 			const userInfo: {
+// 				name: string,
+// 				email: string,
+// 			} = await this.getUserInfo(token);
+
+// 			const session: AuthenticationSession = {
+// 				id: uuid(),
+// 				accessToken: token,
+// 				account: {
+// 					label: userInfo.name,
+// 					id: userInfo.email,
+// 				},
+// 				scopes: []
+// 			};
+// 			await this.context.secrets.store(SESSIONS_SECRET_KEY, JSON.stringify([session]));
+
+// 			this._sessionChangeEmitter.fire({ added: [session], removed: [], changed: [] });
+
+// 			return session;
+// 		} catch (err) {
+// 			window.showErrorMessage(`Sign in failed: ${err}`);
+// 			throw err;
+// 		}
+// 	}
+
+// 	public async removeSession(sessionId: string): Promise<void> {
+// 		const allSessions = await this.context.secrets.get(SESSIONS_SECRET_KEY);
+// 		if (allSessions) {
+// 			let sessions =JSON.parse(allSessions) as AuthenticationSession[];
+// 			const sessionIndex = sessions.findIndex(s => s.id === sessionId);
+// 			const session =sessions[sessionIndex];
+// 			sessions.splice(sessionIndex, 1);
+
+// 			await this.context.secrets.store(SESSIONS_SECRET_KEY, JSON.stringify(sessions));
+
+// 			if (session) {
+// 				this._sessionChangeEmitter.fire({ added: [], removed: [session], changed: [] });
+// 			}
+// 		}
+// 	}
+
+// 	public async dispose() {
+// 		this._disposable.dispose();
+// 	}
+
+// 	private async login(scopes: string[] = []) {
+// 		return await window.withProgress<string>({
+// 			location: ProgressLocation.Notification,
+// 			title: "signing into BeEngineer...",
+// 			cancellable: true,
+// 		}, async (_, token) => {
+// 			const stateId = uuid();
+
+// 			this._pendingStates.push(stateId);
+
+// 			if (!scopes.includes('openId')) {
+// 				scopes.push('openId');
+// 			}
+// 			if (!scopes.includes('profile')) {
+// 				scopes.push('profile');
+// 			}
+// 			if (!scopes.includes('email')) {
+// 			scopes.push('email');
+// 			}
+
+// 			const scopeString = scopes.join(' ');
+
+// 			const searchParams = null;
+// 			const url = Uri.parse('localhost:8000/auth/login/');
+// 			await env.openExternal(url);
+
+// 			let codeExchangePromise = this._codeExchangePromises.get(scopeString);
+// 			if (!codeExchangePromise) {
+// 				codeExchangePromise = promiseFromEvent(this._uriHandler.event, this.handleUri(scopes));
+// 				this._codeExchangePromises.set(scopeString, codeExchangePromise);
+// 			}
+
+// 			try {
+// 				return await Promise.race([
+// 					codeExchangePromise.promise,
+// 					new Promise<string>((_, reject) => setTimeout(() => reject('cancelled'), 60000)),
+// 					promiseFromEvent<any, any>(token.onCancellationRequested, (_: any, _resolvew: any, reject: (arg0: string) => void) => { reject('User cancelled'); }).promise
+// 				]);
+
+
+
+
+// 			} finally {
+// 				this._pendingStates = this._pendingStates.filter(state => state !== stateId);
+// 				codeExchangePromise?.cancel.fire();
+// 				this._codeExchangePromises.delete(scopeString);
+// 			}
+// 		});
+// 	}
+
+// 	private handleUri: (scopes: readonly string[]) => PromiseAdapter<Uri, string> =
+// 	(scopes) => async (uri: { fragment: string | URLSearchParams | Record<string, string | readonly string[]> | Iterable<[string, string]> | readonly [string, string][] | undefined; }, resolve: (arg0: string) => void, reject: (arg0: Error) => void) => {
+// 		const query = new URLSearchParams(uri.fragment);
+// 		const access_token = query.get('access_token');
+// 		const state = query.get('state');
+
+// 		if (!access_token) {
+// 			reject(new Error('Token not found'));
+// 			return;
+// 		}
+// 		if (!state) {
+// 			reject(new Error('Invalid state'));
+// 			return;
+// 		}
+
+// 		if (!this._pendingStates.some(n => n === state)) {
+// 			reject(new Error('state not found'));
+// 			return;
+// 		}
+
+// 		resolve(access_token);
+// 	}
+
+// 	private async getUserInfo(token: string): Promise<any> {
+// 		const res = await fetch(`http://localhost:8000/auth/login/`, {
+// 			headers: { 'Authorization': 'Bearer ' + token}
+// 		});
+// 		return await res.json();
+// 	}
+// }
+
+// // 後で理解する
+// class UriEventHandler extends EventEmitter<Uri> implements UriHandler {
+// 	public handleUri(uri: Uri) {
+// 		this.fire(uri);
+// 	}
+// }
+
 type User = {
 	pk: number;
 	first_name: string;
@@ -62,8 +252,8 @@ export function activate(context: ExtensionContext) {
 				console.log(response);
 			})
 		} else {
-			const username = await window.showInputBox();
-			const password = await window.showInputBox();
+			const username = await window.showInputBox({placeHolder: 'username'});
+			const password = await window.showInputBox({placeHolder: 'password'});
 			const res = await fetch(`http://localhost:8000/auth/login/`, {
 				method: 'POST',
 				headers: {
